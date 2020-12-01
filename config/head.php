44900596b35b82c6789cc2c260a806f146226c0f
Onur Kol <onurkol4161@gmail.com>
2020-12-01 22:46:02 +0300

    Fixed, site title.

--- config/head.php
+++ config/head.php
@@ -5,7 +5,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php echo $siteName; ?></title>
+<title><?php echo $_LANG['string_site_name']; ?></title>
 <link rel="icon" href="/assets/images/favicon.png">
 <link href="/assets/css/main.css" rel="stylesheet" type="text/css">
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
